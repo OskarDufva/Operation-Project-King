3c60cbecd4351ecb2f17721eed9edb4a727b2fd3
Arklashh <schollin.alexander@gmail.com>
2022-10-21 12:00:29 +0200

    Placement script

--- Assets/Scripts/Manager/Tower/PlaceObjectOnGrid.cs
+++ Assets/Scripts/Manager/Tower/PlaceObjectOnGrid.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlaceObjectOnGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform gridCellPrefab;
+    public Transform cube;
+
+    [SerializeField] private int height;
+    [SerializeField] int width;
+    Node[,] nodes;
+
+
     void Start()
+    {
+        CreateGrid();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateGrid()
     {
-        
+        nodes = new Node[width, height];
+        var name = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; i < height; i++)
+            {
+                Vector3 worldPosition = new Vector3(x: 1, y: 0, z: j);
+                Transform obj = Instantiate(gridCellPrefab, worldPosition, Quaternion.identity);
+                obj.name = "Cell" + name;
+                nodes[i, j] = new Node(isPlaceable: true, worldPosition, obj);
+                name++;
+            }
+        }
+
+    }
+}
+
+public class Node
+{
+    public bool isPlaceable;
+    public Vector3 cellPosition;
+    public Transform obj;
+
+    public Node(bool isPlaceable, Vector3 cellPosition, Transform obj)
+    {
+        this.isPlaceable = isPlaceable;
+        this.cellPosition = cellPosition;
+        this.obj = obj;
     }
 }
