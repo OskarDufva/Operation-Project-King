ff4423d7c0481f70c1bed7fce072f88540ac0579
Arklashh <schollin.alexander@gmail.com>
2022-10-25 13:47:26 +0200

    enemy and tower

--- Assets/Scripts/Manager/Enemy.cs
+++ Assets/Scripts/Manager/Enemy.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float maxHealth;
+    public float currentHealth;
+
+    public float damage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -15,4 +20,13 @@ public class Enemy : MonoBehaviour
     {
         
     }
+
+    public void damageHealth()
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth = currentHealth - damage;
+        }
+    }
+
 }
